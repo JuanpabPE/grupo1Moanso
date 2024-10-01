817de8bb71af75d9ec670c97fb94b0619776f50a
Juanpa <pablo.navane23@gmail.com>
2024-10-01 00:55:14 -0500

    diseño

--- ayaM/Form1.Designer.cs
+++ ayaM/Form1.Designer.cs
@@ -28,33 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
             label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(146, 83);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(582, 402);
+            panel1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(242, 125);
+            label1.Location = new Point(42, 87);
             label1.Name = "label1";
-            label1.Size = new Size(33, 15);
+            label1.Size = new Size(177, 20);
             label1.TabIndex = 0;
-            label1.Text = "pene";
-            label1.Click += label1_Click;
+            label1.Text = "Ingresa tu primer número";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 147);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(234, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(210, 156);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            ClientSize = new Size(914, 600);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
+        private Panel panel1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
         private Label label1;
     }
 }
